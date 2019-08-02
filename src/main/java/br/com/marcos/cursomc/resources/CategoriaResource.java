8e0584a4d063f34233cad93caf8e3f94db7970ed
Marcos Sousa <marco.sds@hotmail.com>
2019-08-02 16:19:22 -0300

    Criando Domain e testando Restlet

--- src/main/java/br/com/marcos/cursomc/resources/CategoriaResource.java
+++ src/main/java/br/com/marcos/cursomc/resources/CategoriaResource.java
@@ -1,16 +1,28 @@
 package br.com.marcos.cursomc.resources;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import br.com.marcos.cursomc.domain.Categoria;
 
 @RestController
 @RequestMapping(value="/categorias")
 public class CategoriaResource {
 	
 	@GetMapping
-	public String listar() {
-		return "O REST está Funcionando";
+	public List<Categoria> listar() {
+		
+		Categoria cat1 = new Categoria(1, "Informatica");
+		Categoria cat2 = new Categoria(2, "Escritório");
+		
+		List<Categoria> categorias = new ArrayList<Categoria>();
+		categorias.add(cat1);
+		categorias.add(cat2);
+		return categorias;
 	}
 
 }
