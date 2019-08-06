d344e94cd1d38678997e8d97b3fe9ac27032d6cb
Marcos Sousa <marco.sds@hotmail.com>
2019-08-06 10:16:23 -0300

    criação das entidade de Cidade e Estado

--- src/main/java/br/com/marcos/cursomc/CursomcApplication.java
+++ src/main/java/br/com/marcos/cursomc/CursomcApplication.java
@@ -1,39 +1,46 @@
 package br.com.marcos.cursomc;
 
+import java.util.Arrays;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
+import br.com.marcos.cursomc.domain.Cidade;
+import br.com.marcos.cursomc.domain.Estado;
+import br.com.marcos.cursomc.repositories.CidadeRepository;
+import br.com.marcos.cursomc.repositories.EstadoRepository;
+
 @SpringBootApplication
-public class CursomcApplication {
+public class CursomcApplication implements CommandLineRunner{
 
 	public static void main(String[] args) {
 		SpringApplication.run(CursomcApplication.class, args);
 	}
-//	
-//	@Autowired
-//	private CategoriaRepository categoriaRepository;
-//	
-//	@Autowired
-//	private ProdutoRepository produtoRepositoty;
 	
-//	@Override
-//	public void run(String... args) throws Exception {
-//		Categoria cat1 = new Categoria(null, "Informática");
-//		Categoria cat2 = new Categoria(null, "Escritório");
-//		
-//		Produto p1 = new Produto(null, "Computador", 200.00);
-//		Produto p2 = new Produto(null, "Fone de Ouvido", 400.00);
-//		Produto p3 = new Produto(null, "FOnte de Alimentação", 300.00);
-//		
-//		cat1.getProdutos().addAll(Arrays.asList(p1, p2, p3));
-//		cat2.getProdutos().addAll(Arrays.asList(p2));
-//		
-//		p1.getCategorias().addAll(Arrays.asList(cat1));
-//		p2.getCategorias().addAll(Arrays.asList(cat1, cat2));
-//		p3.getCategorias().addAll(Arrays.asList(cat1));
-//		
-//		categoriaRepository.saveAll(Arrays.asList(cat1, cat2));
-//		produtoRepositoty.saveAll(Arrays.asList(p1,p2,p3));
-//	}
+	@Autowired
+	private EstadoRepository estadoRepository;
+	
+	@Autowired
+	private CidadeRepository cidadeRepository;
+	
+	@Override
+	public void run(String... args) throws Exception {
+		
+		Estado est1 = new Estado(null, "Pará");
+		Estado est2 = new Estado(null, "São Paulo");
+
+		Cidade cid1 = new Cidade(null, "Belém", est1);
+		Cidade cid2 = new Cidade(null, "Capanema", est1);
+		Cidade cid3 = new Cidade(null, "São Paulo", est2);
+
+		est1.getCidades().addAll(Arrays.asList(cid1, cid2));
+		est2.getCidades().addAll(Arrays.asList(cid3));
+		
+		estadoRepository.saveAll(Arrays.asList(est1, est2));
+		cidadeRepository.saveAll(Arrays.asList(cid1, cid2, cid3));
+		
+	}
 
 }
